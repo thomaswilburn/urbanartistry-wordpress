a2f09115d1a0c868fccf24bbf3a4ac9d50231625
Thomas Wilburn <news@thomaswilburn.net>
2014-11-02 15:35:09 -0800

    Fixes for mobile on family page

--- content-family.php
+++ content-family.php
@@ -9,13 +9,13 @@
   $id = get_the_id();
 ?>
 
-<div class="bio-post <?php echo $postWidth ?> content">
+<div class="bio-post <?php echo $postWidth ?> mobile-12 content">
 <h2><?php the_title() ?></h2>
 <h3 class="artistry ua-red"><?php echo get_post_meta($id, "title", true) ?></h3>
 
 <?php if (has_post_thumbnail()) : ?>
   <?php $video = get_post_meta($id, "bio video", true) ?>
-  <div class="mugshot-container <?php if ($video) echo "has-video "; echo $photoWidth; ?>" data-video="<?php echo $video?>">
+  <div class="mugshot-container <?php if ($video) echo "has-video "; echo $photoWidth; ?> mobile-12" data-video="<?php echo $video?>">
 
     <img class="mugshot" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $id ) ); ?>">
 
@@ -25,7 +25,7 @@
 
   </div>
 
-  <div class="grid-6">
+  <div class="grid-6 mobile-12">
     <?php the_content() ?>
     <?php if (get_post_meta($id, "link", true)) : ?>
     <a href="<?php echo get_post_meta($id, "link", true) ?>" class="bio-link">Read more...</a>
